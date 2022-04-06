aac4ee03184b67ae85e42472fd488920a3f287b1
MakotoTaguchi <taguchimakoto@icloud.com>
2022-04-06 18:00:04 +0900

    main

---
+++ chapter2/js/style2.js
@@ -0,0 +1,13 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        color: 'blue',
+        align: 'center',
+        size: '3'
+    },
+    methods: {
+        setAlign: function () {
+            this.align = align;
+        }
+    }
+});
