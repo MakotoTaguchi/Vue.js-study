033bd01d0a64aa128587c79a80a5d8f0e70ecef4
MakotoTaguchi <taguchimakoto@icloud.com>
2022-04-10 20:03:11 +0900

    main

---
+++ chapter4/js/select3.js
@@ -0,0 +1,23 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        clist: [{
+                en: 'ENGLAND',
+                ja: 'イギリス'
+            },
+            {
+                en: 'USA',
+                ja: 'アメリカ'
+            },
+            {
+                en: 'CHINA',
+                ja: '中国'
+            },
+            {
+                en: 'SPAIN',
+                ja: 'スペイン'
+            }
+        ],
+        countries: []
+    }
+});
