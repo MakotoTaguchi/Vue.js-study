985f8c7f22b7129fa9638ad824b4c3cc2803a62f
MakotoTaguchi <taguchimakoto@icloud.com>
2022-04-10 16:07:39 +0900

    main

---
+++ chapter3/js/v-for3.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        customer: {
+            name: '大津真',
+            age: 35,
+            sex: '男',
+            pref: '東京都'
+        }
+    }
+});
