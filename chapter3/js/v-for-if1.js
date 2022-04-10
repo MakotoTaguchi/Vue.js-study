8b4fb1b01edb06acbcf8b93e5ab8d055878feb05
MakotoTaguchi <taguchimakoto@icloud.com>
2022-04-10 16:55:07 +0900

    main

---
+++ chapter3/js/v-for-if1.js
@@ -0,0 +1,36 @@
+var customers = [{
+    id: 1,
+    name: '大竹真',
+    age: 35,
+    sex: '男',
+    pref: '東京都'
+},
+{
+    id: 2,
+    name: '山田太郎',
+    age: 25,
+    sex: '男',
+    pref: '千葉県'
+},
+{
+    id: 3,
+    name: '井上五郎',
+    age: 36,
+    sex: '男',
+    pref: '長野県'
+},
+{
+    id: 4,
+    name: '江藤花子',
+    age: 18,
+    sex: '女',
+    pref: '東京都'
+}
+];
+
+var app = new Vue({
+el: '#app',
+data: {
+    customers: customers
+}
+});
