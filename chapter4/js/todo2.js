984d94e7c0701e77b57567b5a749b1612c6c3c31
MakotoTaguchi <taguchimakoto@icloud.com>
2022-04-30 21:33:23 +0900

    main

---
+++ chapter4/js/todo2.js
@@ -0,0 +1,38 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        storagekey: 'todolist',
+        newtodo: '',
+        todolist: []
+    },
+    methods: {
+        addTodo: function () {
+            if (this.newtodo == '') return;
+            this.todolist.push({
+                text: this.newtodo,
+                done: false,
+                hover: false
+            });
+            this.newtodo = '';
+        },
+        remove: function (index) {
+            if (this.todolist[index].done == true) {
+                this.todolist.splice(index, 1);
+            }
+        }
+    },
+    watch: {
+        todolist: function () {
+            localStorage.setItem(
+                this.storagekey,
+                JSON.stringify(this.todolist)
+            );
+        }
+    },
+    created: function () {
+        var dataStr = localStorage.getItem(this.storagekey);
+        if (dataStr) {
+            this.todolist = JSON.parse(dataStr);
+        }
+    },
+});
